d65f4c59d04710bb05f546b186cfebc960a6c631
gsaboia <gsaboia@yahoo.com>
2024-07-11 18:56:44 -0300

    fix: iters

--- main_with_dropout.py
+++ main_with_dropout.py
@@ -7,7 +7,7 @@ import time
 # hyperparameters
 batch_size = 64
 block_size = 256
-max_iters = 100
+max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
